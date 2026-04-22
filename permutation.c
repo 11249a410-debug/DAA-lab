76291248ca45f28ec49b3ea02571d27c9ee09d16
Vissamsetti Sai Nandini <11249a410@kanchiuniv.ac.in>
2026-04-22 12:00:17 +0530

    Fix permutation function and main logic

--- permutation.c
+++ permutation.c
@@ -1,31 +1,51 @@
+#include <stdio.h>
+
+// Function to generate permutations
 void perm(int arr[], int start, int stop)
 {
-int temp;
-If(start==stop)
-{
-for (int i=0;i<stop;i++) printf(“\t%d”,arr[i]);
-printf(“\n”);
+    int temp;
+
+    if (start == stop)
+    {
+        for (int i = 0; i < stop; i++)
+        {
+            printf("\t%d", arr[i]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        for (int i = start; i < stop; i++)
+        {
+            // Swap
+            temp = arr[i];
+            arr[i] = arr[start];
+            arr[start] = temp;
+
+            // Recursive call
+            perm(arr, start + 1, stop);
+
+            // Backtrack (swap back)
+            temp = arr[i];
+            arr[i] = arr[start];
+            arr[start] = temp;
+        }
+    }
 }
-Else
+
+int main()
 {
-for(int i=start;i<stop;i++)
-{
-temp=arr[i];
-arr[i]=arr[start];
-a[start]=temp;
-perm(arr,start+1;stop);
-temp=arr[i];
-arr[i]=arr[start];
-a[start]=temp;
+    int n, i, a[10];
+
+    printf("Enter total number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    perm(a, 0, n);
+
+    return 0;
 }
-}
-}
-void main()
-{
-int n, I,a[10];
-printf(“Enter total no element”);
-scanf(“%d”,&n);
-for(i=0;i<n;i++)
-scanf(“%d”,&a[i]);
-perm(a, 0,n);
-} 
